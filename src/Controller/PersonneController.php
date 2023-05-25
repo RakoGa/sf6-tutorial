cf337e6239617773b0511316992773973eb6502d
RakoGa <gaetan.rakotomalala38@gmail.com>
2023-05-25 17:13:32 +0200

    lesson 28 completed

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -8,9 +8,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+// préfixer toutes les routes avec /personne
+#[Route('personne')]
 class PersonneController extends AbstractController
 {
-    #[Route('/personne/add', name: 'personne')]
+    #[Route('/', name: 'personne.list')]
+    public function index(ManagerRegistry $doctrine): Response {
+        $repository = $doctrine->getRepository(Personne::class);
+        $personnes = $repository->findAll();
+        return $this->render('personne/index.html.twig', [
+            'personnes' => $personnes
+        ]);
+    }
+
+    #[Route('/add', name: 'personne.add')]
     public function addPersonne(ManagerRegistry $doctrine): Response
     {
         $entityManager = $doctrine->getManager();
