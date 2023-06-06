537b801fe2cde081970df3591c863c2491bdb264
RakoGa <gaetan.rakotomalala38@gmail.com>
2023-06-06 17:27:49 +0200

    lesson 54 completed

--- src/Service/MailerService.php
+++ src/Service/MailerService.php
@@ -5,7 +5,10 @@ use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 
 class MailerService {
-    public function __construct(private MailerInterface $mailer) {}
+    private $replyTo;
+    public function __construct(private MailerInterface $mailer, $replyTo) {
+        $this->replyTo = $replyTo;
+    }
     public function sendEmail(
         $to = 'gaetan.rakotomalala38@gmail.com',
         $content = '<p>See Twig integration for better HTML integration!</p>',
@@ -17,7 +20,7 @@ class MailerService {
             ->to($to)
             //->cc('cc@example.com')
             //->bcc('bcc@example.com')
-            //->replyTo('fabien@example.com')
+            ->replyTo($this->replyTo)
             //->priority(Email::PRIORITY_HIGH)
             ->subject($subject)
             ->text('Sending emails is fun again!')
